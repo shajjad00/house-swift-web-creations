980e3c971aacdad48a237d42517b43bbb1620fb9
soyebsuvo <soyebbeen@gmail.com>
2024-01-18 17:07:25 +0600

    created the contact page

--- src/Route/MyRoute.tsx
+++ src/Route/MyRoute.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home/Home/Home";
+import Contact from "../Pages/Contact/Contact";
 
 const MyRoute = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const MyRoute = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+        path : "/contact",
+        element : <Contact></Contact>
+      }
     ],
   },
 ]);

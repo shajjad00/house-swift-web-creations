fb2eb0062247279a153113abfd23f8102cb94a56
soyebsuvo <soyebbeen@gmail.com>
2024-01-19 16:24:49 +0600

    reverted to older code

--- src/Pages/Home/Banner/Banner.tsx
+++ src/Pages/Home/Banner/Banner.tsx
@@ -12,22 +12,24 @@ const Banner = () => {
     <div>
       <div className="carousel w-full h-[540px] mb-6">
         <div id="slide1" className="carousel-item relative w-full">
-          <img src={banner} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our
-                passion
-              </h2>
-              <p>
-                No matter what the weather, no matter what the situation we are
-                in, if we have the right perspective in life, life will always
-                be beautiful!
-              </p>
-              <div className="flex items-center">
-                <button className="btn mr-5 bg-[#82b440] text-white">
-                  Special Offer
-                </button>
+          <img src={banner} className="w-full" />
+          <div className="absolute flex items-center h-full gap-6 left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+            <div className="max-w-7xl mx-auto px-4 md:px-20 flex justify-start w-full">
+              <div className="text-white space-y-5 w-full">
+                <h2 className="text-3xl md:text-5xl font-bold">
+                  <span className="text-[#fca129]"> Good </span>Service is <br /> our
+                  passion
+                </h2>
+                <p>
+                  No matter what the weather, no matter what the situation we are
+                  in, if we have <br /> the right perspective in life, life will always
+                  be beautiful!
+                </p>
+                <div className="flex items-center">
+                  <button className="uppercase border border-[#09BE51] bg-[#09BE51] hover:bg-transparent text-white py-2 text-lg px-6 hover:border hover:border-[#09BE51] hover:text-[#09BE51] duration-300 cursor-pointer">
+                    Special Offer
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -43,19 +45,26 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img src={img1} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our
-                passion
-              </h2>
-              <p>
-                In a forgotten library, a mysterious book unveils ancient
-                secrets, drawing readers into a realm where shadows intertwine
-                with reality, weaving a spellbinding narrative of enigmas and
-                intrigue.
-              </p>
+          <img src={img1} className="w-full rounded-xl" />
+          <div className="absolute flex items-center h-full gap-6 left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+            <div className="max-w-7xl mx-auto px-4 md:px-20 flex justify-start w-full">
+              <div className="text-white space-y-5 w-full">
+                <h2 className="text-3xl md:text-5xl font-bold">
+                  <span className="text-[#fca129]"> Good </span>Service is <br /> our
+                  passion
+                </h2>
+                <p>
+                  In a forgotten library, a mysterious book unveils ancient
+                  secrets, drawing readers into a realm where shadows <br /> intertwine
+                  with reality, weaving a spellbinding narrative of enigmas and
+                  intrigue.
+                </p>
+                <div className="flex items-center">
+                  <button className="uppercase border border-[#09BE51] bg-[#09BE51] hover:bg-transparent text-white py-2 text-lg px-6 hover:border hover:border-[#09BE51] hover:text-[#09BE51] duration-300 cursor-pointer">
+                    Special Offer
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
           <div className="absolute flex justify-end gap-6 transform -translate-y-1/2 left-5 right-5 bottom-0">
@@ -68,19 +77,26 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img src={img2} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl  font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our
-                passion
-              </h2>
-              <p>
-                Within an ancient tome, cryptic symbols weave tales of enigmatic
-                realms. Readers brave its pages, unlocking mysteries that blur
-                the line between reality and imagination, leaving them
-                spellbound
-              </p>
+          <img src={img2} className="w-full rounded-xl" />
+          <div className="absolute flex items-center h-full gap-6 left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+            <div className="max-w-7xl mx-auto px-4 md:px-20 flex justify-start w-full">
+              <div className="text-white space-y-5 w-full">
+                <h2 className="text-3xl md:text-5xl  font-bold">
+                  <span className="text-[#fca129]"> Good </span>Service is <br /> our
+                  passion
+                </h2>
+                <p>
+                  Within an ancient tome, cryptic symbols weave tales of enigmatic
+                  realms. Readers brave <br /> its pages, unlocking mysteries that blur
+                  the line between reality and imagination, leaving them
+                  spellbound
+                </p>
+                <div className="flex items-center">
+                  <button className="uppercase border border-[#09BE51] bg-[#09BE51] hover:bg-transparent text-white py-2 text-lg px-6 hover:border hover:border-[#09BE51] hover:text-[#09BE51] duration-300 cursor-pointer">
+                    Special Offer
+                  </button>
+                </div>
+              </div>
             </div>
 
           </div>
@@ -94,19 +110,26 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full">
-          <img src={img3} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl  font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our
-                passion
-              </h2>
-              <p>
-                An enthralling novel immerses readers in captivating narratives,
-                exploring diverse lives, love, and challenges. Through vivid
-                characters and compelling plots, it evokes emotions, sparking
-                introspection and profound connections
-              </p>
+          <img src={img3} className="w-full rounded-xl" />
+          <div className="absolute flex items-center h-full gap-6 left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+            <div className="max-w-7xl mx-auto px-4 md:px-20 flex justify-start w-full">
+              <div className="text-white space-y-5 w-full">
+                <h2 className="text-3xl md:text-5xl  font-bold">
+                  <span className="text-[#fca129]"> Good </span>Service is <br /> our
+                  passion
+                </h2>
+                <p>
+                  An enthralling novel immerses readers in captivating narratives,
+                  exploring diverse lives, love, and challenges. <br /> Through vivid
+                  characters and compelling plots, it evokes emotions, sparking
+                  introspection and profound connections
+                </p>
+                <div className="flex items-center">
+                  <button className="uppercase border border-[#09BE51] bg-[#09BE51] hover:bg-transparent text-white py-2 text-lg px-6 hover:border hover:border-[#09BE51] hover:text-[#09BE51] duration-300 cursor-pointer">
+                    Special Offer
+                  </button>
+                </div>
+              </div>
             </div>
 
           </div>
@@ -120,9 +143,10 @@ const Banner = () => {
           </div>
         </div>
         <div id="slide5" className="carousel-item relative w-full">
-          <img src={img4} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
+          <img src={img4} className="w-full rounded-xl" />
+          <div className="absolute flex items-center h-full gap-6 left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+            <div className="max-w-7xl mx-auto px-4 md:px-20 flex justify-start w-full">
+              <div className="text-white space-y-5 w-full">
               <h2 className="text-3xl md:text-5xl  font-bold">
                 <span className="text-[#fca129]"> Good </span>Service is our <br />
                 passion

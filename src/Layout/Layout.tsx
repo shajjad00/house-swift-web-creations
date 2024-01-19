0ebef75686a5f959a1a47f492bfb577cf31894d2
amir811537 <amirhossain.bc.75@gmail.com>
2024-01-19 12:26:32 +0600

    about us and footer added

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Footer/Footer";
 
 const Layout = () => {
   return (
     <>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 };

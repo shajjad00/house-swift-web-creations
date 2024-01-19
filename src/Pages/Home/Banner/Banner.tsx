7eac42516901d5f32f7fa9a3c5a01e3a75b3e914
shajjad00 <shajjadph@gmail.com>
2024-01-19 16:27:28 +0600

    fix all bug

--- src/Pages/Home/Banner/Banner.tsx
+++ src/Pages/Home/Banner/Banner.tsx
@@ -9,7 +9,6 @@ const Banner = () => {
   return (
     <div>
       <div className="carousel w-full h-[540px] mb-6">
-<<<<<<< HEAD
         <div
           id="slide1"
           className="carousel-item relative w-full"
@@ -35,25 +34,6 @@ const Banner = () => {
                     Special Offer
                   </button>
                 </div>
-=======
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src={banner} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our
-                passion
-              </h2>
-              <p>
-                No matter what the weather, no matter what the situation we are
-                in, if we have the right perspective in life, life will always
-                be beautiful!
-              </p>
-              <div className="flex items-center">
-                <button className="btn mr-5 bg-[#82b440] text-white">
-                  Special Offer
-                </button>
->>>>>>> a16cc7906e2675be46dab862517fe88f9690299f
               </div>
             </div>
           </div>
@@ -73,7 +53,6 @@ const Banner = () => {
             </a>
           </div>
         </div>
-<<<<<<< HEAD
         <div
           id="slide2"
           className="carousel-item relative w-full"
@@ -101,22 +80,6 @@ const Banner = () => {
                   </button>
                 </div>
               </div>
-=======
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src={img1} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our
-                passion
-              </h2>
-              <p>
-                In a forgotten library, a mysterious book unveils ancient
-                secrets, drawing readers into a realm where shadows intertwine
-                with reality, weaving a spellbinding narrative of enigmas and
-                intrigue.
-              </p>
->>>>>>> a16cc7906e2675be46dab862517fe88f9690299f
             </div>
           </div>
           <div className="absolute flex justify-end gap-6 transform -translate-y-1/2 left-5 right-5 bottom-0">
@@ -134,7 +97,6 @@ const Banner = () => {
             </a>
           </div>
         </div>
-<<<<<<< HEAD
         <div
           id="slide3"
           className="carousel-item relative w-full"
@@ -162,22 +124,6 @@ const Banner = () => {
                   </button>
                 </div>
               </div>
-=======
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src={img2} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl  font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our
-                passion
-              </h2>
-              <p>
-                Within an ancient tome, cryptic symbols weave tales of enigmatic
-                realms. Readers brave its pages, unlocking mysteries that blur
-                the line between reality and imagination, leaving them
-                spellbound
-              </p>
->>>>>>> a16cc7906e2675be46dab862517fe88f9690299f
             </div>
           </div>
           <div className="absolute flex justify-end gap-6 transform -translate-y-1/2 left-5 right-5 bottom-0">
@@ -195,7 +141,6 @@ const Banner = () => {
             </a>
           </div>
         </div>
-<<<<<<< HEAD
         <div
           id="slide4"
           className="carousel-item relative w-full"
@@ -224,22 +169,6 @@ const Banner = () => {
                   </button>
                 </div>
               </div>
-=======
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src={img3} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl  font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our
-                passion
-              </h2>
-              <p>
-                An enthralling novel immerses readers in captivating narratives,
-                exploring diverse lives, love, and challenges. Through vivid
-                characters and compelling plots, it evokes emotions, sparking
-                introspection and profound connections
-              </p>
->>>>>>> a16cc7906e2675be46dab862517fe88f9690299f
             </div>
           </div>
           <div className="absolute flex justify-end gap-6 transform -translate-y-1/2 left-5 right-5 bottom-0">
@@ -257,7 +186,6 @@ const Banner = () => {
             </a>
           </div>
         </div>
-<<<<<<< HEAD
         <div
           id="slide5"
           className="carousel-item relative w-full"
@@ -281,23 +209,6 @@ const Banner = () => {
                   revealing the depths of the human spirit.
                 </p>
                 <div className="flex items-center">
-=======
-        <div id="slide5" className="carousel-item relative w-full">
-          <img src={img4} className="w-full " />
-          <div className="absolute flex items-center h-full gap-6   left-0 right-0 bottom-0  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
-            <div className="text-white space-y-5 pl-12 w-1/2">
-              <h2 className="text-3xl md:text-5xl  font-bold">
-                <span className="text-[#fca129]"> Good </span>Service is our <br />
-                passion
-              </h2>
-              <p>
-                In a world of emotions, a drama book unfolds human complexities,
-                depicting love, betrayal, and <br /> resilience. Characters navigate
-                life is twists, evoking empathy, and revealing the depths of the
-                human spirit.
-              </p>
-              <div className="flex items-center">
->>>>>>> a16cc7906e2675be46dab862517fe88f9690299f
                   <button className="uppercase border border-[#09BE51] bg-[#09BE51] hover:bg-transparent text-white py-2 text-lg px-6 hover:border hover:border-[#09BE51] hover:text-[#09BE51] duration-300 cursor-pointer">
                     Special Offer
                   </button>
@@ -321,10 +232,6 @@ const Banner = () => {
           </div>
         </div>
       </div>
-<<<<<<< HEAD
-=======
-      
->>>>>>> a16cc7906e2675be46dab862517fe88f9690299f
     </div>
   );
 };
--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -1,8 +1,4 @@
 import { Helmet } from "react-helmet-async";
-<<<<<<< HEAD
-=======
-// import AboutUs from "../../AboutUs/AboutUs";
->>>>>>> a16cc7906e2675be46dab862517fe88f9690299f
 import Banner from "../Banner/Banner";
 import Blog from "../Blog/Blog";
 
@@ -13,11 +9,7 @@ const Home = () => {
         <title>House Swift | Home</title>
       </Helmet>
       <Banner />
-<<<<<<< HEAD
-=======
-      {/* <AboutUs></AboutUs> */}
-      <Blog />
->>>>>>> a16cc7906e2675be46dab862517fe88f9690299f
+      <Blog></Blog>
     </div>
   );
 };

f287bb94af6c8e0e1577ce6351d0f5a98926c052
Rakib <mohammadmohiuddin3490@gmai.com>
2024-02-01 03:54:43 +0600

    edited

--- src/hook/useAxiosPublic.tsx
+++ src/hook/useAxiosPublic.tsx
@@ -3,7 +3,6 @@ import axios from "axios";
 const axiosPublic = axios.create({
   baseURL: "https://house-swift-web-creations-server.vercel.app",
 });
-// https://house-swift-web-creations-server.vercel.app
 
 const useAxiosPublic = () => {
   return axiosPublic;
